929e3467c44e6f505dd3bc40990f98da95f9d73f
kmurray30 <kyle.murray100@gmail.com>
2017-01-15 22:18:37 -0500

    Mostly working

---
+++ Diagnostic Kit 2/ResultsView.swift
@@ -0,0 +1,47 @@
+//
+//  ResultsView.swift
+//  Diagnostic Kit 2
+//
+//  Created by Kyle Murray on 1/15/17.
+//  Copyright © 2017 Kyle Murray. All rights reserved.
+//
+
+import UIKit
+
+class ResultsView: UIViewController {
+
+    @IBOutlet weak var warning: UIImageView!
+    @IBOutlet weak var range: UILabel!
+    @IBOutlet weak var details: UILabel!
+    var amount = 20
+    var receptor = ""
+    var threshold = 10
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        details.text = "Sample conatins \(amount) ppb \(receptor) antigen"
+        range.text = "Acceptible range is 0-\(amount) ppb"
+        if (amount <= threshold) {
+            warning.isHidden = true
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
