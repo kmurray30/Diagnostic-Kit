f90f72d028b4f6fc405ad329b5ea61a4d4917880
kmurray30 <kyle.murray100@gmail.com>
2017-01-15 20:06:28 -0500

    Initial Commit

---
+++ Diagnostic Kit 2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Diagnostic Kit 2
+//
+//  Created by Kyle Murray on 1/15/17.
+//  Copyright © 2017 Kyle Murray. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
